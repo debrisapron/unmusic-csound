843a2c42f879a8eb3bceb403fa9fea94d65763bb
DEBRIS APRON <debrisapron@gmail.com>
2018-07-11 18:09:15 -0700

    Get CLI working

--- bin/umcs.js
+++ bin/umcs.js
@@ -8,4 +8,5 @@ let inputFile = process.argv[2]
 let outputFile = process.argv[3]
 let js = fs.readFileSync(inputFile, { encoding: 'UTF8' })
 let csScore = csScoreFromJs(js)
+console.log(csScore)
 fs.writeFileSync(outputFile, csScore, { encoding: 'UTF8' })
