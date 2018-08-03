c73b9c1271088d93c1483699ab0fa21de9667649
DEBRIS APRON <debrisapron@gmail.com>
2018-08-03 08:22:01 -0700

    Add error logging to file

--- bin/umcs.js
+++ bin/umcs.js
@@ -4,6 +4,11 @@
 let fs = require('fs')
 let { csScoreFromJs } = require('..')
 
+process.on('uncaughtException', (err) => {
+  fs.writeFileSync('error.log', err.message, { encoding: 'UTF8' })
+  throw err
+});
+
 let inputFile = process.argv[2]
 let outputFile = process.argv[3]
 let js = fs.readFileSync(inputFile, { encoding: 'UTF8' })
