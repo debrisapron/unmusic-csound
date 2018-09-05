34a51ee6b5407e63daf1c1a2cc27b009ae8d6ee8
DEBRIS APRON <debrisapron@gmail.com>
2018-09-05 16:05:33 -0700

    Use unmusic-core from npm

--- bin/umcs.js
+++ bin/umcs.js
@@ -1,17 +1,16 @@
 #!/usr/bin/env node
-'use strict'
+"use strict";
 
-let fs = require('fs')
-let { csScoreFromJs } = require('..')
+let fs = require("fs");
+let { csScoreFromJs } = require("..");
 
-process.on('uncaughtException', (err) => {
-  fs.writeFileSync('error.log', err.message, { encoding: 'UTF8' })
-  throw err
+process.on("uncaughtException", err => {
+  fs.writeFileSync("error.log", err.message, { encoding: "UTF8" });
+  throw err;
 });
 
-let inputFile = process.argv[2]
-let outputFile = process.argv[3]
-let js = fs.readFileSync(inputFile, { encoding: 'UTF8' })
-let csScore = csScoreFromJs(js)
-console.log(csScore)
-fs.writeFileSync(outputFile, csScore, { encoding: 'UTF8' })
+let inputFile = process.argv[2];
+let outputFile = process.argv[3];
+let js = fs.readFileSync(inputFile, { encoding: "UTF8" });
+let csScore = csScoreFromJs(js);
+fs.writeFileSync(outputFile, csScore, { encoding: "UTF8" });
--- index.js
+++ index.js
@@ -1,55 +1,55 @@
-'use strict'
-let requireFromString = require('require-from-string')
-let um = require('unmusic-core')
+"use strict";
+let requireFromString = require("require-from-string");
+let um = require("unmusic-core");
 
-let twelveTet = (nn, ref = 440) => nn && Math.pow(2, (nn - 69) / 12) * ref
+let twelveTet = (nn, ref = 440) => nn && Math.pow(2, (nn - 69) / 12) * ref;
 
-let csParamsFromUmPayload = (payload) => {
+let csParamsFromUmPayload = payload => {
   let indexes = Object.keys(payload)
-    .filter((key) => key.match(/^p[0-9]+$/))
-    .map((key) => parseInt(key.slice(1)))
-    .filter((index) => index > 4)
-  if (!indexes.length) return ''
+    .filter(key => key.match(/^p[0-9]+$/))
+    .map(key => parseInt(key.slice(1)))
+    .filter(index => index > 4);
+  if (!indexes.length) return "";
 
-  let len = Math.max(...indexes) - 4
-  let arr = Array(len).fill(0)
-  indexes.forEach((index) => (arr[index - 5] = payload[`p${index}`]))
-  return arr.join(' ')
-}
+  let len = Math.max(...indexes) - 4;
+  let arr = Array(len).fill(0);
+  indexes.forEach(index => (arr[index - 5] = payload[`p${index}`]));
+  return arr.join(" ");
+};
 
-let csActionFromUmAction = (umAction) => {
-  let { type, payload } = umAction
-  if (type === 'NOOP') return
+let csActionFromUmAction = umAction => {
+  let { type, payload } = umAction;
+  if (type === "NOOP") return;
   let {
     nn,
     time,
     dur,
     handlers: [instr]
-  } = payload
-  let freq = twelveTet(nn)
-  instr = isNaN(instr) ? `"${instr}"` : instr
-  let params = csParamsFromUmPayload(payload)
-  return `i ${instr} ${time} ${dur} ${freq} ${params}`.trim()
-}
+  } = payload;
+  let freq = twelveTet(nn);
+  instr = isNaN(instr) ? `"${instr}"` : instr;
+  let params = csParamsFromUmPayload(payload);
+  return `i ${instr} ${time} ${dur} ${freq} ${params}`.trim();
+};
 
-let csScoreFromUmScore = (umScore) => {
-  let csLines = []
-  let { actions, tempo } = umScore
-  if (tempo) csLines.push(`t 0 ${tempo}`)
-  actions.forEach((action) => csLines.push(csActionFromUmAction(action)))
-  return csLines.filter((x) => x).join('\n')
-}
+let csScoreFromUmScore = umScore => {
+  let csLines = [];
+  let { actions, tempo } = umScore;
+  if (tempo) csLines.push(`t 0 ${tempo}`);
+  actions.forEach(action => csLines.push(csActionFromUmAction(action)));
+  return csLines.filter(x => x).join("\n");
+};
 
-let csScoreFromJs = (script) => {
+let csScoreFromJs = script => {
   let js = `
     module.exports = (um) => {
-      let { ${Object.keys(um).join(', ')} } = um;
+      let { ${Object.keys(um).join(", ")} } = um;
       ${script}
     }
-  `
-  let scoreFn = requireFromString(js)
-  let umScore = scoreFn(um)
-  return csScoreFromUmScore(umScore)
-}
+  `;
+  let scoreFn = requireFromString(js);
+  let umScore = scoreFn(um);
+  return csScoreFromUmScore(umScore);
+};
 
-module.exports = { csScoreFromJs }
+module.exports = { csScoreFromJs };
